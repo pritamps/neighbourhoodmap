0229e923704bb6461e7b2939418c94539cf57aa5
Pritam Sukumar <pritamps@gmail.com>
2017-05-01 11:09:56 +0530

    First commit eh.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include gulp plugins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+ // Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+      .pipe(concat('main.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+// Watch for changes in files
+gulp.task('watch', function() {
+   // Watch .js files
+  gulp.watch(src + 'js/*.js', ['scripts']);
+ });
+
+ // Default Task
+gulp.task('default', ['scripts', 'watch']);
